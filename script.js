59a549614f85e81f2f1460540e6a683c3927e080
RamDevgit99 <ilovemomoveryverymuch@gmail.com>
2026-02-03 17:22:20 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function yesClicked() {
+    document.getElementById("response").innerHTML =
+        "YAYYY 💖💖💖<br>You just made me the happiest person ever ✨";
+}
+
+function noClicked() {
+    document.getElementById("response").innerHTML =
+        "Press Yes!! 💜<br> Its okay, you can still press Yes and you're still amazing 🌸";
+}
